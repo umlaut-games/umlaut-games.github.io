497facf2c7a1457d662b2788441fafb14e5080ce
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-08-01 20:46:57 +0200

    initial version

---
+++ mänsion/precache-manifest.32d5ed17ce6374161f8eabaf2cf344da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c7f1dfef5d3365689df5422da75e9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e3c2b8b2ef167544d07",
+    "url": "/static/css/main.212f5e9f.chunk.css"
+  },
+  {
+    "revision": "78dbc42ce914ddc29f82",
+    "url": "/static/js/2.2ae17104.chunk.js"
+  },
+  {
+    "revision": "4c31d9d424778d5d442a00a637de3d7a",
+    "url": "/static/js/2.2ae17104.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e3c2b8b2ef167544d07",
+    "url": "/static/js/main.b806730b.chunk.js"
+  },
+  {
+    "revision": "65c6c96e5b92c5686494",
+    "url": "/static/js/runtime-main.ab88f792.js"
+  }
+]);
