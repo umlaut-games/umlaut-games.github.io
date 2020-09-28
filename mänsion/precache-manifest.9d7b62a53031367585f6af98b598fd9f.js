d0a45224712db5107c884293e34fd42abf2a34da
H34D <13647606+H34D@users.noreply.github.com>
2020-09-28 19:11:57 +0000

    Deploying to master from  @ 8e329b414e4820b8c799e9798e80789b31040716 🚀

---
+++ mänsion/precache-manifest.9d7b62a53031367585f6af98b598fd9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9b0a9eca34e2dfced90488c978be68",
+    "url": "./index.html"
+  },
+  {
+    "revision": "743c8905d13bde5d35aa",
+    "url": "./static/css/main.212f5e9f.chunk.css"
+  },
+  {
+    "revision": "d9f185996b33c11621fd",
+    "url": "./static/js/2.c7ab1281.chunk.js"
+  },
+  {
+    "revision": "1708ef9b5ad16294183ca569f8921a25",
+    "url": "./static/js/2.c7ab1281.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743c8905d13bde5d35aa",
+    "url": "./static/js/main.b540ebb2.chunk.js"
+  },
+  {
+    "revision": "6727b2d55d002e195e31",
+    "url": "./static/js/runtime-main.e0847e8c.js"
+  }
+]);
--- mänsion/service-worker.js
+++ mänsion/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f7384b649ff404229ec5a56ca93eaa60.js"
+  "./precache-manifest.9d7b62a53031367585f6af98b598fd9f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
