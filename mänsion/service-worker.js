2888a4010ff89df05800804883b65d9a4e50202b
H34D <13647606+H34D@users.noreply.github.com>
2020-10-23 14:50:47 +0000

    Deploying to master from  @ fd61a6a4ffe06824c95bb77bbec548d20f143e33 🚀

--- mänsion/service-worker.js
+++ mänsion/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.231b62dd655d0e34277ee4f049242816.js"
+  "./precache-manifest.9c6f2707208e73d1d8ba892c2d58d02c.js"
 );
 
 self.addEventListener('message', (event) => {
