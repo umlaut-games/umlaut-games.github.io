e93d8b3020e9cb4b3a2c3d3fd5f0a02246c80b95
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2023-04-21 10:35:07 +0200

    update mänsion

--- mänsion/maension/assets/assets_dist/shaders/frag-light.glsl
+++
@@ -1,9 +0,0 @@
-#ifdef GL_ES
-precision highp float;
-#endif
-
-uniform vec4 uGlobalColor;
-
-void main() {
-    gl_FragColor = uGlobalColor;
-}
--- mänsion/maension/assets/assets_dist/shaders/vert-2d.glsl
+++
@@ -1,7 +0,0 @@
-/*
- * The main program
- */
-void main() {
-    // Vertex shader output
-    gl_Position = vec4(1.0);
-}
